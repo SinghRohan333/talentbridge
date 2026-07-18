55b66ce1bd249cd8af408f09a1dc9e6a94cbc3ac
SinghRohan333 <rohansingh12345602042@gmail.com>
2026-07-18 20:03:47 +0600

    utils modified

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getErrorMessage(
+  error: unknown,
+  fallback = "Something went wrong",
+): string {
+  if (axios.isAxiosError(error)) {
+    return (
+      (error.response?.data as { error?: string } | undefined)?.error ??
+      fallback
+    );
+  }
+  return fallback;
 }
