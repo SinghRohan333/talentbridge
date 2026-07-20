b78fb9150aa38a9ae7f470407f40e23c5376ee17
SinghRohan333 <rohansingh12345602042@gmail.com>
2026-07-20 16:57:46 +0600

    main layout updated

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
+import { ChatWidget } from "@/components/chat/ChatWidget";
 
 export default function MainLayout({
   children,
@@ -11,6 +12,7 @@ export default function MainLayout({
       <Navbar />
       <div className="flex-1">{children}</div>
       <Footer />
+      <ChatWidget />
     </div>
   );
 }
